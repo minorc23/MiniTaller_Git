2b4d3b17c0a58c2d32fa677561d7df518eca7312
Minor Campos <mecb2019@estudiantec.cr>
2024-03-22 06:19:59 -0600

    Add file with python tasks

---
+++ python_tasks.py
@@ -0,0 +1,11 @@
+# 1. Declare una variable
+
+# 2. Crear un diccionario con nombres de frutas y su cantidad
+
+# 3. Crear una lista con los números del 1 al 10
+
+# 4. Crear una función que tome un número y devuelva su cuadrado
+
+# 5. Crear una clase llamada 'Auto' con atributos 'marca' y 'modelo', y un método para imprimir estos atributos
+
+# 6. Imprimir un Hello World
